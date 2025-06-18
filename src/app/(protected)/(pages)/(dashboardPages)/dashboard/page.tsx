f1876f87f0df2b4993a126cfc6ee6c645a5285b4
Narendra yadav <95286237+Narendray12@users.noreply.github.com>
2025-06-18 12:51:34 +0530

    Update Dashboard to Show Projects

--- src/app/(protected)/(pages)/(dashboardPages)/dashboard/page.tsx
+++ src/app/(protected)/(pages)/(dashboardPages)/dashboard/page.tsx
@@ -2,24 +2,26 @@ import { getAllProjects } from "@/actions/projects";
 import NotFound from "@/components/global/not-found";
 import Project from "@/components/global/project";
 import React from "react";
+
 const DashboardPage = async () => {
   const allProjects = await getAllProjects();
+  
   return (
     <div className="w-full flex flex-col gap-6 relative p-4 md:p-0">
-      <div className="flex flex-col-reverse items-start w-full gap=6 sm:flex-row sm:justif-between sm:items-center">
-        <div className="flex flex-col item-start">
+      <div className="flex flex-col-reverse items-start w-full gap-6 sm:flex-row sm:justify-between sm:items-center">
+        <div className="flex flex-col items-start">
           <h1 className="text-2xl font-semibold dark:text-primary backdrop-blur-lg">
             Projects
           </h1>
           <p className="text-base font-normal dark:text-secondary">
-            All of your work is one place
+            All of your work in one place
           </p>
         </div>
       </div>
+      
       {/* Projects */}
       {allProjects.data && allProjects.data.length > 0 ? (
-        // <Project projects={allProjects.data} />
-        ""
+        <Project projects={allProjects.data} />
       ) : (
         <NotFound />
       )}
